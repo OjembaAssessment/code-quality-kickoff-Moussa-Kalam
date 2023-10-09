e5806e8f807b2b142e5d8b1c0789d4cd28ac3c4f
Moussa Kalam AMZAT <m.amzat@alustudent.com>
2023-10-09 15:06:21 +0200

    feat: Return the largest number the an array of numbers

--- src/improvement-3.js
+++ src/improvement-3.js
@@ -6,11 +6,6 @@
 
 // FUNCTION 3
 export default function findLargestNumber(numbers) {
-  let largest = numbers[0];
-  for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] > largest) {
-      largest = numbers[i];
-    }
-  }
-  return largest;
+  // Return the largest number in the numbers array
+  return Math.max(...numbers);
 }
