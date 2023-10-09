d8ae3c8b09b3644a0c60a6fb23375aca0c82258e
Moussa Kalam AMZAT <m.amzat@alustudent.com>
2023-10-09 17:14:54 +0200

    feat: Implement password penalty count.

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -7,10 +7,37 @@
  */
 export default function penaltyPoints(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
-  if (typeof password !== "string") password = String(password);
 
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  // * * * INSERT YOUR CODE HERE * * * * * * * * * * * * * *
-  // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-  //
+  // Early return is password is falsy
+  if (!password) return 0;
+
+  /**
+   * If password contains two consecutive characters,
+   * map each one of them to 1 and get their sum
+   *
+   * If password contains three or more consecutive characters,
+   * map each one of them to 2 and get their sum
+   */
+  if (/(\w)\1\1+/g.test(password)) {
+    return password
+      .match(/(\w)\1\1+/g)
+      .map((v) => 2)
+      .reduce((a, b) => a + b, 0);
+  } else if (/(\w)\1/g.test(password)) {
+    return password
+      .match(/(\w)\1/g)
+      .map((v) => 1)
+      .reduce((a, b) => a + b, 0);
+  } else {
+    return 0;
+  }
 }
+
+// My test
+console.log(penaltyPoints("U2jSS277pQ"));
+console.log(penaltyPoints("amGGAg8m0Q"));
+console.log(penaltyPoints("70vSSS859Q"));
+console.log(penaltyPoints("70vSSS85DDDD9Q"));
+console.log(penaltyPoints("amdGAg8m0Q"));
+console.log(penaltyPoints(""));
+console.log(penaltyPoints(null));
