7b01e404b25a014d0ade4d2c9439ded981b9eafe
Moussa Kalam AMZAT <m.amzat@alustudent.com>
2023-10-09 20:23:12 +0200

    feat: Implement password validator

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -8,7 +8,9 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
  */
 export default function isValidPassword(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
-  if (typeof password !== "string") password = String(password);
+  if (typeof password !== "string") {
+    password = String(password);
+  }
 
   // * * * YOUR CODE GOES IN HERE ... * * *
   /*
@@ -20,6 +22,20 @@ export default function isValidPassword(password = "") {
    *   return ...;
    * }
    */
+
+  // Check password with a directly ascending/descending sequence of number
+  if (/01|12|23|34|45|56|67|78|89|98|87|76|65|54|43|32|21|10/.test(password))
+    return false;
+
+  // Check if password has the appropriate length
+  // If password includes numbers and letters (uppercase and lowercase)
+  // Password does not include any special characters
+  if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])[a-zA-Z\d]{10}$/.test(password))
+    return false;
+
+  // Check that forbidden passwords are not considered
+  if (forbiddenPasswords.includes(password)) return false;
+
   const setOfPassword = new Set([...password]);
   if (setOfPassword.size < 4) return false;
   return true;
